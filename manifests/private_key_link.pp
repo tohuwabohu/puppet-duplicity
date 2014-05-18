929bd837b34eea5c4235aa6b79d3d01d48e82404
Martin Meinhold <Martin.Meinhold@gmx.de>
2014-05-18 18:56:50 +1000

    Fix broken file link

--- manifests/private_key_link.pp
+++ manifests/private_key_link.pp
@@ -28,7 +28,7 @@ define duplicity::private_key_link($ensure = present) {
   }
 
   file { "${duplicity::params::duply_config_dir}/${profile}/gpgkey.${keyid}.sec.asc":
-    ensure  => file,
+    ensure  => link,
     target  => "${duplicity::params::duply_private_key_dir}/${keyid}.asc",
     require => Duplicity::Private_Key[$keyid],
   }
