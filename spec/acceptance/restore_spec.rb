8cfbc45f0c55082792ba49da0123b0b45d916742
Martin Meinhold <Martin.Meinhold@gmx.de>
2016-11-27 16:22:38 +1100

    Update acceptance tests
    Use the same provider to create the backup; otherwise we run into weird
    "not authoritative at backend" error on restore.

--- spec/acceptance/restore_spec.rb
+++ spec/acceptance/restore_spec.rb
@@ -4,57 +4,85 @@ describe 'duplicity::file can restore from backup' do
   before {
     shell('rm -rf /tmp/{duplicity,restore-me}')
     shell('mkdir -p /tmp/restore-me/dummy')
-    apply_manifest("
-          class { 'duplicity':
-            backup_target_url      => 'file:///tmp/duplicity/',
-            backup_target_username => 'john',
-            backup_target_password => 'doe',
-          }
-
-          duplicity::profile { 'system':
-            gpg_encryption => false,
-          }
-
-          duplicity::file { '/tmp/restore-me':
-            ensure => backup,
-          }
-    ", :catch_failures => true)
-    shell('duply system backup')
-    shell('rm -rf /tmp/restore-me')
   }
 
-  specify 'with default package provider' do
-    manifest = <<-EOS
-          class { 'duplicity':
-            backup_target_url      => 'file:///tmp/duplicity/',
-            backup_target_username => 'john',
-            backup_target_password => 'doe',
-          }
+  describe 'with default package provider' do
+    before {
+      apply_manifest("
+            class { 'duplicity':
+              backup_target_url      => 'file:///tmp/duplicity/',
+              backup_target_username => 'john',
+              backup_target_password => 'doe',
+            }
 
-          duplicity::profile { 'system':
-            gpg_encryption => false,
-          }
+            duplicity::profile { 'system':
+              gpg_encryption => false,
+            }
 
-          duplicity::file { '/tmp/restore-me': }
-    EOS
-    apply_manifest(manifest, :catch_failures => true)
+            duplicity::file { '/tmp/restore-me':
+              ensure => backup,
+            }
+      ", :catch_failures => true)
+      shell('duply system backup')
+      shell('rm -rf /tmp/restore-me')
+    }
+
+    specify 'should provision with no errors' do
+      manifest = <<-EOS
+            class { 'duplicity':
+              backup_target_url      => 'file:///tmp/duplicity/',
+              backup_target_username => 'john',
+              backup_target_password => 'doe',
+            }
+
+            duplicity::profile { 'system':
+              gpg_encryption => false,
+            }
+
+            duplicity::file { '/tmp/restore-me': }
+      EOS
+      apply_manifest(manifest, :catch_failures => true)
+    end
   end
 
-  specify 'with archive package provider' do
-    manifest = <<-EOS
-          class { 'duplicity':
-            duply_package_provider => 'archive',
-            backup_target_url      => 'file:///tmp/duplicity/',
-            backup_target_username => 'john',
-            backup_target_password => 'doe',
-          }
+  describe 'with archive package provider' do
+    before {
+      apply_manifest("
+            class { 'duplicity':
+              duply_package_provider => 'archive',
+              backup_target_url      => 'file:///tmp/duplicity/',
+              backup_target_username => 'john',
+              backup_target_password => 'doe',
+            }
 
-          duplicity::profile { 'system':
-            gpg_encryption => false,
-          }
+            duplicity::profile { 'system':
+              gpg_encryption => false,
+            }
 
-          duplicity::file { '/tmp/restore-me': }
-    EOS
-    apply_manifest(manifest, :catch_failures => true)
+            duplicity::file { '/tmp/restore-me':
+              ensure => backup,
+            }
+      ", :catch_failures => true)
+      shell('duply system backup')
+      shell('rm -rf /tmp/restore-me')
+    }
+
+    specify 'should provision with no errors' do
+      manifest = <<-EOS
+            class { 'duplicity':
+              duply_package_provider => 'archive',
+              backup_target_url      => 'file:///tmp/duplicity/',
+              backup_target_username => 'john',
+              backup_target_password => 'doe',
+            }
+
+            duplicity::profile { 'system':
+              gpg_encryption => false,
+            }
+
+            duplicity::file { '/tmp/restore-me': }
+      EOS
+      apply_manifest(manifest, :catch_failures => true)
+    end
   end
 end
