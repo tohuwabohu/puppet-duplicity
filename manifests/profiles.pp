75e392d24d74a29df33d577d442de6fc7044c3d3
Jochem van Dieten <jochem@vandieten.net>
2017-07-16 12:05:42 +0200

    Helper class to be able to create profiles through Foreman

---
+++ manifests/profiles.pp
@@ -0,0 +1,22 @@
+# == Class: duplicity::profiles
+#
+# Configure profiles from a hash.
+#
+# === Parameters
+#
+# [*profiles*]
+#   A hash with profiles to create through duplicity::profile
+# === Authors
+#
+# Jochem van Dieten <jochem@vandieten.net>
+#
+# === Copyright
+#
+# Copyright 2017 Jochem van Dieten
+#
+class duplicity::profiles(
+  $profiles                 = {},
+) {
+  include ::duplicity
+  create_resources('duplicity::profile', $profiles)
+}
