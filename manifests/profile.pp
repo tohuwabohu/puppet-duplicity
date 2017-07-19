88db3f5afaa5c1366b482031d23756f85fdef03d
Jochem van Dieten <jochem@vandieten.net>
2017-07-19 15:44:25 +0200

    Remove the check for S3 keys

--- manifests/profile.pp
+++ manifests/profile.pp
@@ -177,19 +177,9 @@ define duplicity::profile(
     fail("Duplicity::Profile[${title}]: gpg_encryption must be true or false")
   }
 
-  if $target =~ /^s3:/ and empty($duply_environment) and (versioncmp($duply_version, '1.11') <= 0) {
-    notify{ "Duplicity::Profile[${title}]: S3 warning":
-      message => 'S3 targets now require AWS_ACCCESS_KEY_ID & AWS_SECRET_ACCESS_KEY env vars to be set - setting for you',
-    }
-    $real_duply_environment = [
-      "export AWS_ACCESS_KEY_ID='${target_username}'",
-      "export AWS_SECRET_ACCESS_KEY='${target_password}'",
-    ]
-  } else {
-    $real_duply_environment = empty($duply_environment) ? {
-      true    => [],
-      default => any2array($duply_environment)
-    }
+  $real_duply_environment = empty($duply_environment) ? {
+    true    => [],
+    default => any2array($duply_environment)
   }
 
   $real_gpg_encryption_keys = empty($gpg_encryption_keys) ? {
