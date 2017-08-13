741f0111bfb8a5a0db3fedb6606b151ea5704a15
Martin Meinhold <Martin.Meinhold@gmx.de>
2017-08-13 19:04:28 +1000

    Update beaker

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,23 +1,18 @@
-require 'beaker-rspec/spec_helper'
+require 'beaker-rspec'
 require 'beaker/puppet_install_helper'
+require 'beaker/module_install_helper'
 
-if default.name =~ /ubuntu.*1604.*/
-  # https://tickets.puppetlabs.com/browse/BKR-821
-  run_puppet_install_helper(type='agent')
-else
-  run_puppet_install_helper
-end
+run_puppet_install_helper
+install_module_on(hosts)
+install_module_from_forge_on(hosts, 'puppetlabs-stdlib', '= 4.15.0')
+install_module_from_forge_on(hosts, 'puppetlabs-concat', '= 2.2.0')
+install_module_from_forge_on(hosts, 'camptocamp-archive', '= 0.9.0')
+install_module_from_forge_on(hosts, 'yo61-logrotate', '= 1.4.0')
 
 RSpec.configure do |c|
-  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
   c.formatter = :documentation
+
   c.before :suite do
-    puppet_module_install(:source => proj_root, :module_name => 'duplicity')
-    hosts.each do |host|
-      on host, puppet('module', 'install', 'puppetlabs-stdlib', '--version 4.15.0')
-      on host, puppet('module', 'install', 'puppetlabs-concat', '--version 2.2.0')
-      on host, puppet('module', 'install', 'camptocamp-archive', '--version 0.9.0')
-      on host, puppet('module', 'install', 'yo61-logrotate', '--version 1.4.0')
-    end
+    logger.info("Using Puppet version #{(on default, 'puppet --version').output.chomp}")
   end
 end
