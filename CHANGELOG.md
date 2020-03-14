693a7922356610f040dd9f64b41c368f189abbe7
Martin Meinhold <Martin.Meinhold@gmx.de>
2020-03-14 19:21:25 +0100

    Update Puppet modules to next major version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [6.0.0] - 2020-03-14
+### Changed
+
+- Update Puppet modules to their latest major version, in particular concat and stdlib are now on 6.x. As a result,
+  Puppet 4.x has officially been dropped
+ 
 ## [5.1.1] - 2020-03-14
 ### Changed
 
--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -5,10 +5,10 @@ require 'beaker/module_install_helper'
 
 run_puppet_install_helper
 install_module_on(hosts)
-install_module_from_forge_on(hosts, 'puppetlabs-stdlib', '= 5.2.0')
-install_module_from_forge_on(hosts, 'puppetlabs-concat', '= 5.3.0')
-install_module_from_forge_on(hosts, 'puppet-archive', '= 3.2.1')
-install_module_from_forge_on(hosts, 'puppet-logrotate', '= 3.4.0')
+install_module_from_forge_on(hosts, 'puppetlabs-stdlib', '= 6.2.0')
+install_module_from_forge_on(hosts, 'puppetlabs-concat', '= 6.2.0')
+install_module_from_forge_on(hosts, 'puppet-archive', '= 4.4.0')
+install_module_from_forge_on(hosts, 'puppet-logrotate', '= 4.0.0')
 
 RSpec.configure do |c|
   c.formatter = :documentation
