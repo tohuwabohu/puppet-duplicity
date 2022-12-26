44c77abcb44a2f2b4f59d04538950c57de1ccbfc
Martin Meinhold <Martin.Meinhold@gmx.de>
2022-12-26 14:49:53 +0100

    Update dependencies

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -6,10 +6,10 @@ require 'beaker/module_install_helper'
 # collection: puppet - latest, puppet6 is the current version
 install_puppet_agent_on(hosts, {:puppet_collection => 'puppet' } )
 install_module_on(hosts)
-install_module_from_forge_on(hosts, 'puppetlabs-stdlib', '= 6.6.0')
-install_module_from_forge_on(hosts, 'puppetlabs-concat', '= 6.4.0')
-install_module_from_forge_on(hosts, 'puppet-archive', '= 4.6.0')
-install_module_from_forge_on(hosts, 'puppet-logrotate', '= 5.0.0')
+install_module_from_forge_on(hosts, 'puppetlabs-stdlib', '= 8.5.0')
+install_module_from_forge_on(hosts, 'puppetlabs-concat', '= 7.3.0')
+install_module_from_forge_on(hosts, 'puppet-archive', '= 6.1.0')
+install_module_from_forge_on(hosts, 'puppet-logrotate', '= 6.1.0')
 
 RSpec.configure do |c|
   c.formatter = :documentation
